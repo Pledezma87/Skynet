951fa7418665cbd91eb5f7356629b1ada89daaed
Pablo Cesar Ledezma <pablo.ledezma.br@gmail.com>
2023-09-19 12:54:39 +0200

    Schema CRUD  Users $ Form

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,12 +1,67 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  role: { type: String, enum: ["User", "Admin"], default: "User" },
-  email: { type: String, unique: true },
-  password: String,
-  // Otros campos específicos del administrador, si es necesario
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return validator.isAlpha(value, "en-US", { ignore: " -" });
+      },
+      message: "El nombre debe contener solo letras y espacios."
+    }
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return validator.isAlpha(value, "en-US", { ignore: " -" });
+      },
+      message: "El apellido debe contener solo letras y espacios."
+    }
+  },
+
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    default: "User",
+    trim: true,
+    lowercase: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return validator.isEmail(value);
+      },
+      message: "El formato del correo electrónico no es válido."
+    }
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 12,
+    validate: {
+      validator: function (value) {
+        return /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\-\\/]+$/u.test(value);
+      },
+      message: "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial."
+    }
+  }
 });
 
 const User = mongoose.model("User", userSchema);
